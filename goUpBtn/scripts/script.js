42d98cc2bf39bdcdc95d80e78c871179daf93068
Shahrzad <shahrzadjkoushesh@gmail.com>
2023-04-04 12:30:35 +0430

    add click function

--- goUpBtn/scripts/script.js
+++ goUpBtn/scripts/script.js
@@ -1,11 +1,17 @@
 $(document).ready(function(){
     let topbtn = $('.top-btn');
     $(window).scroll(function(){
-        if ($(window).scrollTop() > 100){
+        if ($(window).scrollTop() > 200){
             topbtn.fadeIn();
         }
         else{
             topbtn.fadeOut();
         }
+    });
+
+    topbtn.click(function(){
+        $('html').animate({
+            scrollTop: 0
+        }, 1000);
     });
 });
